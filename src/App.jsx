39d7057258ec1c77bf6e6a998858a1686fa4cbda
muzammil <muzammilabid08@gmail.com>
2026-09-02 00:49:07 +0500

    fix github pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,45 @@ import "./App.css";
 import { Country } from "./pages/Country";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
-import { Contact} from "./pages/Contact";
+import { Contact } from "./pages/Contact";
 import { AppLayout } from "./components/Layout/AppLayout";
 import { ErrorPage } from "./pages/ErrorPage";
 import { CountryDetails } from "./components/Layout/CountryDetails";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "country",
+          element: <Country />,
+        },
+        {
+          path: "country/:id",
+          element: <CountryDetails />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <AppLayout />,
-    errorElement:<ErrorPage/>,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "country",
-        element: <Country />,
-      },
-      {
-        path: "country/:id",
-        element: <CountryDetails />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-        // action:getFormData,
-      },
-    ],
+    basename: "/react-countries-project",
   },
-]);
+);
 const App = () => {
   return <RouterProvider router={router}></RouterProvider>;
 };
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react_countries_project",
+  base: "/react-countries-project/",
 })
